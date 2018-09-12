2b7ec14d0a7e26c7b1b6b1091a197626e13cab7e
Matt Styles <matt@personalurban.com>
2018-09-12 21:27:53 +0100

    use commonJS export

--- index.js
+++ index.js
@@ -20,25 +20,23 @@ db.lightblue = 'rgb(109, 194, 202)'
 db.yellow = 'rgb(218, 212, 94)'
 db.white = 'rgb(222, 238, 214)'
 
-db = [
-  db.black,
-  db.purple,
-  db.darkblue,
-  db.darkgrey,
-  db.brown,
-  db.darkgreen,
-  db.red,
-  db.grey,
-  db.blue,
-  db.orange,
-  db.lightgrey,
-  db.green,
-  db.pink,
-  db.lightblue,
-  db.yellow,
-  db.white
-]
+db[0] = db.black
+db[1] = db.purple
+db[2] = db.darkblue
+db[3] = db.darkgrey
+db[4] = db.brown
+db[5] = db.darkgreen
+db[6] = db.red
+db[7] = db.grey
+db[8] = db.blue
+db[9] = db.orange
+db[10] = db.lightgrey
+db[11] = db.green
+db[12] = db.pink
+db[13] = db.lightblue
+db[14] = db.yellow
+db[15] = db.white
 
-export default {
+module.exports = {
   db: db
 }
