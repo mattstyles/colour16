ce908ab6439e59aeb4031b6c0ddf089f6bc796cb
Matt Styles <matt@personalurban.com>
2018-09-12 21:20:20 +0100

    add darkbringer palette

---
+++ index.js
@@ -0,0 +1,44 @@
+
+/* http://pixeljoint.com/forum/forum_posts.asp?TID=12795 */
+/* (c) DarkBringer */
+
+var db = []
+db.black = 'rgb(20, 12, 28)'
+db.purple = 'rgb(68, 36, 52)'
+db.darkblue = 'rgb(48, 52, 109)'
+db.darkgrey = 'rgb(78, 74, 78)'
+db.brown = 'rgb(133, 76, 48)'
+db.darkgreen = 'rgb(52, 101, 36)'
+db.red = 'rgb(208, 70, 72)'
+db.grey = 'rgb(117, 113, 97)'
+db.blue = 'rgb(89, 125, 206)'
+db.orange = 'rgb(210, 125, 44)'
+db.lightgrey = 'rgb(133, 149, 161)'
+db.green = 'rgb(109, 170, 44)'
+db.pink = 'rgb(210, 170, 153)'
+db.lightblue = 'rgb(109, 194, 202)'
+db.yellow = 'rgb(218, 212, 94)'
+db.white = 'rgb(222, 238, 214)'
+
+db = [
+  db.black,
+  db.purple,
+  db.darkblue,
+  db.darkgrey,
+  db.brown,
+  db.darkgreen,
+  db.red,
+  db.grey,
+  db.blue,
+  db.orange,
+  db.lightgrey,
+  db.green,
+  db.pink,
+  db.lightblue,
+  db.yellow,
+  db.white
+]
+
+export default {
+  db: db
+}
