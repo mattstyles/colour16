de2dc0916ec68dd9529ab830103b057e974cc7c9
Matt Styles <matt@personalurban.com>
2018-09-13 06:17:20 +0100

    add c64 palette

---
+++ c64.js
@@ -0,0 +1,40 @@
+
+/* http://androidarts.com/palette/16pal.htm */
+/* (c) Arne Niklas Jansson */
+
+var c64 = []
+c64.void = 'rgb(0, 0, 0)'
+c64.ash = 'rgb(157, 157, 157)'
+c64.blind = 'rgb(255, 255, 255)'
+c64.bloodred = 'rgb(190, 38, 51)'
+c64.pigmeat = 'rgb(224, 111, 139)'
+c64.oldbrown = 'rgb(73, 60, 43)'
+c64.newbrown = 'rgb(164, 100, 34)'
+c64.blaze = 'rgb(235, 137, 49)'
+c64.zornskin = 'rgb(247, 226, 107)'
+c64.shadegreen = 'rgb(47, 72, 78)'
+c64.leafgreen = 'rgb(68, 137, 26)'
+c64.slimegreen = 'rgb(163, 206, 39)'
+c64.nightblue = 'rgb(27, 38, 50)'
+c64.seablue = 'rgb(0, 87, 132)'
+c64.skyblue = 'rgb(49, 162, 242)'
+c64.cloudblue = 'rgb(178, 220, 239)'
+
+c64[0] = c64.black
+c64[1] = c64.purple
+c64[2] = c64.darkblue
+c64[3] = c64.darkgrey
+c64[4] = c64.brown
+c64[5] = c64.darkgreen
+c64[6] = c64.red
+c64[7] = c64.grey
+c64[8] = c64.blue
+c64[9] = c64.orange
+c64[10] = c64.lightgrey
+c64[11] = c64.green
+c64[12] = c64.pink
+c64[13] = c64.lightblue
+c64[14] = c64.yellow
+c64[15] = c64.white
+
+module.exports = c64
--- index.js
+++ index.js
@@ -1,42 +1,8 @@
 
-/* http://pixeljoint.com/forum/forum_posts.asp?TID=12795 */
-/* (c) DarkBringer */
-
-var db = []
-db.black = 'rgb(20, 12, 28)'
-db.purple = 'rgb(68, 36, 52)'
-db.darkblue = 'rgb(48, 52, 109)'
-db.darkgrey = 'rgb(78, 74, 78)'
-db.brown = 'rgb(133, 76, 48)'
-db.darkgreen = 'rgb(52, 101, 36)'
-db.red = 'rgb(208, 70, 72)'
-db.grey = 'rgb(117, 113, 97)'
-db.blue = 'rgb(89, 125, 206)'
-db.orange = 'rgb(210, 125, 44)'
-db.lightgrey = 'rgb(133, 149, 161)'
-db.green = 'rgb(109, 170, 44)'
-db.pink = 'rgb(210, 170, 153)'
-db.lightblue = 'rgb(109, 194, 202)'
-db.yellow = 'rgb(218, 212, 94)'
-db.white = 'rgb(222, 238, 214)'
-
-db[0] = db.black
-db[1] = db.purple
-db[2] = db.darkblue
-db[3] = db.darkgrey
-db[4] = db.brown
-db[5] = db.darkgreen
-db[6] = db.red
-db[7] = db.grey
-db[8] = db.blue
-db[9] = db.orange
-db[10] = db.lightgrey
-db[11] = db.green
-db[12] = db.pink
-db[13] = db.lightblue
-db[14] = db.yellow
-db[15] = db.white
+const db = require('./db')
+const c64 = require('./c64')
 
 module.exports = {
-  db: db
+  db: db,
+  c64: c64
 }
