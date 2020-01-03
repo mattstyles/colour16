b97bc0c8c41a5ba5fa4ad2b19d9ad13aa39b3029
Matt Styles <matt@personalurban.com>
2020-01-03 19:04:47 +0000

    add crayola-based ansi palette

---
+++ ansiCrayola.js
@@ -0,0 +1,37 @@
+
+var acr = []
+acr.black = '#303236'
+acr.red = '#ED0A3F'
+acr.green = '#3AA655'
+acr.yellow = '#FBE870'
+acr.blue = '#4570E6'
+acr.magenta = '#F653A6'
+acr.cyan = '#76D7EA'
+acr.white = '#BFC0C4'
+acr.blackBright = '#646569'
+acr.redBright = '#FE4C40'
+acr.greenBright = '#93DFB8'
+acr.yellowBright = '#FCD667'
+acr.blueBright = '#4997D0'
+acr.magentaBright = '#FF3399'
+acr.cyanBright = '#00CCCC'
+acr.whiteBright = '#E5E8EF'
+
+acr[0] = acr.black
+acr[1] = acr.red
+acr[2] = acr.green
+acr[3] = acr.yellow
+acr[4] = acr.blue
+acr[5] = acr.magenta
+acr[6] = acr.cyan
+acr[7] = acr.white
+acr[8] = acr.blackBright
+acr[9] = acr.redBright
+acr[10] = acr.greenBright
+acr[11] = acr.yellowBright
+acr[12] = acr.blueBright
+acr[13] = acr.magentaBright
+acr[14] = acr.cyanBright
+acr[15] = acr.whiteBright
+
+module.exports = acr
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 const db = require('./db')
 const c64 = require('./c64')
+const acr = require('./acr')
 
 module.exports = {
   db: db,
-  c64: c64
+  c64: c64,
+  acr: acr
 }
